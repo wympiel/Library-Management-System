1fcf014144def97b269728e7bce4570c26b1be9a
wympiel <wymp10@gmail.com>
2020-11-10 22:46:41 +0100

    third commit

--- AddBook.py
+++ AddBook.py
@@ -32,7 +32,6 @@ def addBook():
     root.minsize(width=400, height=400)
     root.geometry("600x500")
 
-
     mypass = "root"
     mydatabase = "library"
 
@@ -40,7 +39,56 @@ def addBook():
     cur = con.cursor()
 
     # Enter Table Names here
-    bookTable = "books" # Book Table
+    bookTable = "books"  # Book Table
 
     Canvas1 = Canvas(root)
-    
+
+    Canvas1.config(bg="#ff6e40")
+    Canvas1.pack(expand=True, fill=BOTH)
+
+    headingFrame1 = Frame(root, bg="#ffbb00", bd=5)
+    headingFrame1.place(relx=0.25, rely=0.1, relwidth=0.5, relheight=0.13)
+
+    headingLabel = Label(headingFrame1, text="Add Books", bg='blue', fg='white',
+                         font=('Courier', 15))  # kolor w był black
+    headingLabel.place(relx=0, rely=0, relwidth=1, relheight=1)
+
+    labelFrame = Frame(root, bg='black')
+    labelFrame.place(relx=0.1, rely=0.4, relwidth=0.8, relheight=0.4)
+
+    # Book ID
+    lb1 = Label(LabelFrame, text="Book ID: ", bg='black', fg='white')
+    lb1.place(relx=0.05, rely=0.2, relheight=0.08)
+
+    bookInfo1 = Entry(labelFrame)
+    bookInfo1.place(relx=0.3, rely=0.2, relwidth=0.62, relheight=0.08)
+
+    # Title
+    lb2 = Label(LabelFrame, text="Title: ", bg='black', fg='white')
+    lb2.place(relx=0.05, rely=0.35, relheight=0.08)
+
+    bookInfo2 = Entry(labelFrame)
+    bookInfo2.place(relx=0.3, rely=0.2, relwidth=0.62, relheight=0.08)
+    # Book Author
+
+    lb3 = Label(LabelFrame, text="Author: ", bg='black', fg='white')
+    lb3.place(relx=0.05, rely=0.50, relheight=0.08)
+
+    bookInfo3 = Entry(labelFrame)
+    bookInfo3.place(relx=0.3, rely=0.50, relwidth=0.62, relheight=0.08)
+
+    # Book Status
+    lb4 = Label(LabelFrame, text="Status (available/issued): ", bg='black', fg='white')
+    lb4.place(relx=0.05, rely=0.65, relheight=0.08)
+
+    bookInfo4 = Entry(labelFrame)
+    bookInfo4.place(relx=0.3, rely=0.65, relwidth=0.62, relheight=0.08)
+
+    # Submit Button
+    SubmitBtn = Button(root, text="Submit", bg='#d1ccc0', fg='black', command=bookRegister)
+    SubmitBtn.place(relx=0.28, rely=0.9, relwidth=0.18, relheight=0.08)
+
+    quitBtn = Button(root, text="Quit", bg='f7f1e3', fg='black', command=root.destroy)
+    quitBtn.place(relx=0.53, rely=0.9, relwidth=0.18, relheight=0.08)
+
+    root.manloop()
