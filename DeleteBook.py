19a03766dd81f2a9f336974cd6f0e3d5cb7dc057
wympiel <wymp10@gmail.com>
2020-11-11 23:06:25 +0100

    last improvement

--- DeleteBook.py
+++ DeleteBook.py
@@ -46,4 +46,31 @@ def delete():
     root.geometry("600x500")
 
 
-    
+    Canvas1 = Canvas(root)
+    Canvas1.config(bg="#006b38")
+    Canvas1.pack(expand=True, fill=BOTH)
+
+    headingFrame1 = Frame(root, bg="#ffbb00", bd=5)
+    headingFrame1.place(relx=0.25, rely=0.1, relwidth=0.5, relheight=0.13)
+
+    headingLabel = Label(headingFrame1, text="Delete Book", bg='black', fg='white', font=('Courier', 15))
+    headingLabel.place(relx=0, rely=0, relwidth=1, relheight=1)
+
+    labelFrame = Frame(root, bg='black')
+    labelFrame.place(relx=0.1, rely=0.3, relwidth=0.8, relheight=0.5)
+
+    # Book ID to Delete
+    lb2 = Label(LabelFrame, text="Book ID: ", bg='black', fg='white')
+    lb2.place(relx=0.05, rely=0.5)
+
+    bookInfo1 = Entry(labelFrame)
+    bookInfo1.place(relx=0.3, rely=0.5, relwidth=0.62)
+
+    # Submit Button
+    SubmitBtn = Button(root, text="Submit", bg='#d1ccc0', fg='black', command=deleteBook)
+    SubmitBtn.place(relx=0.28, rely=0.9, relwidth=0.18, relheight=0.08)
+
+    quitBtn = Button(root, text="Quit", bg='#f7f1e3', fg='black', command=root.destroy)
+    quitBtn.place(relx=0.53, rely=0.9, relwidth=0.18, relheight=0.08)
+
+    root.mainloop()
