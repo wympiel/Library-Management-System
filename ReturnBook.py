1a9ec1ca77abe295f42041e05f409b13e128d503
wympiel <wymp10@gmail.com>
2020-11-15 12:25:51 +0100

    second commit

--- ReturnBook.py
+++ ReturnBook.py
@@ -44,4 +44,41 @@ def return():
         else:
             messagebox.showinfo("Error", "Book ID not present")
     except:
-        messagebox.showinfo()
+        messagebox.showinfo("Error", "Can't fetch Book ID's")
+
+    issueSql = "delete from" +issueTable+ "where bid = '" +bid+ "'"
+
+    print(bid in  allBid)
+    print(status)
+    updateStatus = "update " +bookTable+ "set status = 'avail' where bid = '" +bid+ "'"
+    try:
+        if bid in allBid and status == True:
+            cur.execute(issueSql)
+            con.commit()
+            cur.execute(updateStatus)
+            con.commit()
+            messagebox.showinfo('Success', "Book Returned Successfully")
+        else:
+            allBid.clear()
+            messagebox.showinfo('Message', "Plkease check the book ID")
+            root.destroy()
+            return
+    except:
+        messagebox.showinfo("Search Error", "The value entered is wrong. Try again")
+
+    allBid.clear()
+    root.destroy()
+
+def returnBook():
+    global bookInfo1, SubmitBtn, quitBtn, Canvas1, con, cur, root, labelFrame, lb1
+
+    root = Tk()
+    root.title("Library")
+    root.minsize(width=400, height=400)
+    root.geometry("600x500")
+
+    Canvas1 = Canvas(root)
+
+    Canvas1.config(bg="#006b38")
+    Canvas1.pack(expand=True, fill=BOTH)
+    
